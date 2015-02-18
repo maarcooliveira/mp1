932de6dcf85f9b49553a80efd58d593a1f0835c7
Marco Oliveira <oliveira.marco.andre@gmail.com>
2015-02-18 07:57:09 +0000

    MP1 finished

--- js/script.js
+++ js/script.js
@@ -1,62 +1,66 @@
 function init() {
 
-	var prevsection;
+	// Carousel values
+	const change_img_time = 5000;
+	const transition_speed = 0;
+	const color_banner_1 = "#b53f51";
+	const color_banner_2 = "#6aa84f";
+	const color_banner_3 = "#ff9900";
 
-	// Resizing navbar solution based on: http://callmenick.com/2014/02/18/create-an-animated-resizing-header-on-scroll/
+	var listItems = $("#carousel").children('li');
+	var listLen = listItems.length;
+	var current;
+	var changeTimeout;
+
+	// Navbar links
+	var sections = [];
+
+	$('nav a').each(function(index){
+		sections.push($(this).attr('href'));
+	});
+
+	// Scroll listener
 	window.addEventListener('scroll', function(e) {
-		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-			shrinkOn = 300;
-		if (distanceY > shrinkOn) {
-			$("#navbar").addClass("smaller");
-			$("#title").text("Marco Oliveira");
-		} 
-		else {
-			if ($("#navbar").hasClass("smaller")){
-				$("#navbar").removeClass("smaller");
-				$("#title").text("Marco Andre De Oliveira");
-			}
-		}
-
-		// //code reference: http://jsfiddle.net/traverso85/d2HaW/25/
-		// $('nav a').each(function(index){
-		//     var section = $(this).attr('href');
-
-		//     if ($(window).scrollTop() >= ($(section).offset().top) - 120) {
-		//     	if (prevsection !== section) {
-		//     		$('nav a').removeClass('selected');
-		   //          $('nav a[href='+section+']').addClass('selected');
-		   //          prevsection = section;
-		//     	}
-		//     }	
-		//     if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-		//         $('nav a').removeClass('selected');
-		//         $('nav a:last').addClass('selected');
-		//     }
-		// });
+		resizeNavbar();
+		indicatePosition();
 	});
 
+	// Carousel listeners
+	$('#slide-left').on('click', function(){
+  		moveTo('prev');
+  	});
 
-	$('.modal-link').on('click', function(){
-		// $('body').css("overflow", "hidden");
-		$('#modal').css("visibility", "visible");
+  	$('#slide-right').on('click', function(){
+  		moveTo('next');
+  	});
+
+  	// Modal listeners
+  	$('.modal-link').on('click', function(){
+		$("#" + this.id + "-modal").css("visibility", "visible");
+		$('body').css("overflow", "hidden");
 	});
 
-	$('#modal').on('click', function(){
-		$('#modal').css("visibility", "hidden");
-	});
-
-	$('#modal div').on('click', function(e){
-		e.stopPropagation();
+	$('.modal').on('click', function(){
+		closeModal();
 	});
 
 	$(document).keyup(function(e) { 
 	    if (e.keyCode == 27) { 
-	        $("#modal").css("visibility", "hidden");
+	        closeModal();
 	    } 
 	});
 
+	$('.modal div').on('click', function(e){
+		e.stopPropagation();
+	});
 
-	// smooth scroll solution based on: http://stackoverflow.com/questions/4198041/jquery-smooth-scroll-to-an-anchor
+	function closeModal() {
+		$(".modal").css("visibility", "hidden");
+		$('body').css("overflow", "scroll");
+	}
+
+	// Navbar click listener - Smooth scroll
+	// Smooth scroll solution based on: http://stackoverflow.com/questions/4198041/jquery-smooth-scroll-to-an-anchor
 	$("nav a").click(function(event) {
 		event.preventDefault();
 		
@@ -70,9 +74,87 @@ function init() {
 			dest = $(this.hash).offset().top;
 		}
 
+		// Removes the navbar size of the destination; navbar will be bigger (120) if the destination is "INTRO"
 		dest = dest - navbar_size;
-		$('html,body').animate({scrollTop:dest}, 1000,'swing');
+		$('html,body').animate({scrollTop:dest}, 1000, 'swing');
 	});
+
+	// Resizing navbar solution based on: http://callmenick.com/2014/02/18/create-an-animated-resizing-header-on-scroll/
+	// The page title on the navbar will also change depending on its size
+	function resizeNavbar() {
+		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+			shrinkOn = 300;
+		if (distanceY > shrinkOn) {
+			$("#navbar").addClass("smaller");
+			$("#title").text("Marco Oliveira");
+		} 
+		else if ($("#navbar").hasClass("smaller")){
+			$("#navbar").removeClass("smaller");
+			$("#title").text("Marco Andre De Oliveira");
+		}
+	}
+
+	// Position indication reference: http://jsfiddle.net/traverso85/d2HaW/25/
+	function indicatePosition() {
+		for (var i = sections.length - 1; i >= 0; i--) {
+			var section = sections[i];
+		    if ($(window).scrollTop() >= ($(section).offset().top) - 120) {
+	    		$('nav a').removeClass('selected');
+	            $('nav a[href='+section+']').addClass('selected');
+		    	break;
+		    }	
+		    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+		        $('nav a').removeClass('selected');
+		        $('nav a:last').addClass('selected');
+		        break;
+		    }
+		}
+	}
+
+	// Carousel functions
+	// Reference: http://www.theodinproject.com/javascript-and-jquery/creating-an-image-carousel-slider
+  	function moveTo(newIndex) {
+	    var index = newIndex;
+
+	    if (newIndex === 'prev') {
+	        index = (current > 0) ? (current - 1) : (listLen - 1);
+	    }
+		else if (newIndex === 'next') {
+	        index = (current < listLen - 1) ? (current + 1) : 0;
+	    }
+
+	    listItems.fadeOut(transition_speed)
+	             .eq(index).fadeIn(transition_speed);
+
+	    current = index;
+
+	    changeColors(current);
+
+	    clearTimeout(changeTimeout);
+	    changeTimeout = setTimeout(function() { 
+	    	moveTo('next'); 
+	    }, change_img_time);
+  	};
+
+  	function changeColors(slide) {
+  		var color;
+
+  		if (slide === 0) {
+  			color = color_banner_1;
+  		}
+  		else if (slide === 1) {
+  			color = color_banner_2;
+  		}
+  		else {
+  			color = color_banner_3;
+  		}
+
+  		$('#about').css('background-color', color);
+  	}
+
+  	// Initialize interface behavior
+  	indicatePosition();
+  	moveTo('next');
 }
 
 window.onload = init();
